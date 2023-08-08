568ed485757e59cca76e95543dd7f7fc5b034fd3
Ala Younes <ala.younes@atos.net>
2023-08-08 10:04:36 +0200

    feat: day 1

---
+++ randomNumberGeneratorInRange.js
@@ -0,0 +1,5 @@
+const randomNumberGeneratorInRange = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+console.log(`My random number: ${randomNumberGeneratorInRange(5, 100)}`);
